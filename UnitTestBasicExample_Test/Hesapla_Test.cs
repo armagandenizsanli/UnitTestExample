f75bc4e2eb43bb5eb01a6d3dc09d4510a91228aa
armagan.sanli.mdbf15@iste.edu.tr <armagan.sanli.mdbf15@iste.edu.tr>
2020-01-21 09:38:37 +0300

    Add project files.

---
+++ UnitTestBasicExample_Test/Hesapla_Test.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+using UnitTestBasicExample;
+
+namespace UnitTestBasicExample_Test
+{
+    [TestFixture]
+    public class Hesapla_Test
+    {
+        private UnitTestBasicExample.Hesapla _nesne;
+         [SetUp]
+        public void Setup()
+        {
+            _nesne = new Hesapla();
+
+        }
+        [TestCase(3,5)]
+        [TestCase(2,7)]
+        [TestCase(5,5)]
+        [Ignore("Ignore a fixture")]
+        public void Topla_Test_True(int a,int b)
+        {
+            var result = _nesne.Topla(a, b);
+            Assert.AreEqual(8, result,"Hata");
+            //StringAssert.Contains("a", "b");
+            
+            
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _nesne = null;
+        }
+    }
+}
---
+++ UnitTestBasicExample_Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestBasicExample_Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestBasicExample_Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a878dc69-67fe-49bd-8395-f7a2fc56505d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
